00ccbeb65474888bcada47282e7288a3a98d8b97
kdpena2 <65924620+kdpena2@users.noreply.github.com>
2022-05-27 16:09:28 -0500

    Create servidorweb.js

---
+++ servidorweb.js
@@ -0,0 +1,10 @@
+const http = requiere('http')
+const port=3000;
+const server=http.createServer((req,res)=>{
+  res.statusCode=200;
+  res.setHeader('Content-Type','text/plain');
+  res.end('Hola Mundo');
+});
+server.listen(port,()=>{
+  console.log("Server running on port:${port}`);
+});
